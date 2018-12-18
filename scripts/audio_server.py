45165c754ad60b4d974aa6a776cb041daf3fb445
Anas Abou Allaban <piraka9011@hotmail.com>
2018-12-18 10:12:19 -0500

    Bug fixes x2 (#12)
    * None type fix 1
    
    * Debug check
    
    * added contexts support
    
    * clean up
    
    * Sentiment and pep8 refactor
    
    * Snowboy and output audio
    
    * Finished audio synthesis along with CMake build fix
    
    * Comments
    
    * Cleanup
    
    * Added event inputs
    
    * Events and refactoring
    
    * Some changes?
    
    * hotword launch and param unicode fix
    
    * unicode x2
    
    * Param msg creation and output formatting
    
    * a7a
    
    * Streaming return order
    
    * Forgot to convert ros_df context to df context
    
    * Refactoring, too many errors
    
    * Unknown exception error fix
    This was caused by me being silly and not refactoring properly.
    Future me should note that this issue occurs when I'm not sending a request properly.
    
    * Added ability to send list params
    
    * Merge
    
    * cpp event test and fix
    
    * add list f-nality

--- scripts/audio_server.py
+++ scripts/audio_server.py
@@ -21,13 +21,13 @@ class AudioServer:
 
         self._server_name = rospy.get_param('/dialogflow_client/server_name',
                                             '127.0.0.1')
-
         self._port = rospy.get_param('/dialogflow_client/port', 4444)
 
         rospy.loginfo("DF_CLIENT: Audio Server Started!")
 
     def _connect(self):
-        """Create a socket, listen on the server:port and wait for a connection."""
+        """Create a socket, listen on the server:port and wait for a connection.
+        """
         self.serversocket.bind((self._server_name, self._port))
         rospy.loginfo("DF_CLIENT: Waiting for connection...")
         self.serversocket.listen(1)
@@ -35,9 +35,9 @@ class AudioServer:
     def _callback(self, in_data, frame_count, time_info, status):
         """PyAudio callback to continuously get audio data from the mic and put
         it in a buffer.
-        :param in_data: Audio data received from mic.
-        :return: A tuple with a signal to keep listening to audio input device
-        :rtype: tuple(None, int)
+         :param in_data: Audio data received from mic.
+         :return: A tuple with a signal to keep listening to audio input device
+         :rtype: tuple(None, int)
         """
         for s in self.read_list[1:]:
             s.send(in_data)
@@ -51,12 +51,14 @@ class AudioServer:
             while True:
                 # select() waits until an object is readable. Here this means
                 # it will wait until there is data to be read from the socket
-                readable, writable, errored = select.select(self.read_list, [], [])
+                readable, writable, errored = select.select(self.read_list,
+                                                            [], [])
                 for s in readable:
                     if s is self.serversocket:
                         (clientsocket, address) = self.serversocket.accept()
                         self.read_list.append(clientsocket)
-                        rospy.loginfo("DF_CLIENT: Connection from {}".format(address))
+                        rospy.loginfo("DF_CLIENT: Connection from {}".format(
+                                address))
                     else:
                         data = s.recv(1024)
                         if not data:
--- src/dialogflow_ros/utils/converters.py
+++ src/dialogflow_ros/utils/converters.py
@@ -15,8 +15,11 @@ def parameters_struct_to_msg(parameters):
     :rtype: (list of DialogflowParameter) or None
     """
     if parameters.items():
-        return [DialogflowParameter(param_name=str(name), value=str(value))
-                for name, value in parameters.items()]
+        param_list = []
+        for name, value in parameters.items():
+            param = DialogflowParameter(param_name=str(name), value=[str(value)])
+            param_list.append(param)
+        return param_list
     else:
         return []
 
@@ -43,8 +46,9 @@ def events_msg_to_struct(event, language_code='en-US'):
     :return: Dialogflow EventInput to send
     :rtype: EventInput
     """
+    parameters = params_msg_to_struct(event.parameters)
     return EventInput(name=event.event_name,
-                      parameters=event.parameters,
+                      parameters=parameters,
                       language_code=language_code)
 
 
